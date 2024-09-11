6844a8b40270a66c9c8900f8d528ee802eabc44e
seoyeon252 <novsnow@sookmyung.ac.kr>
2024-09-11 14:38:15 +0900

    실습4

--- main.c
+++ main.c
@@ -4,6 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int a;
+	int b;
+	a = 10;
+	b = 20;
+	
 	printf("Hello, Sookmyung Electronics! ");
+	printf("%d + %d = %d", a, b, a+b);
+	
 	return 0;
 }
